6f80fdd02f2bb1ef08452039c1d128431aa4eb63
imaginos <imaginos@726aef4b-f618-498e-8847-2d620e286838>
2008-03-17 15:30:28 +0000

    could be a global config line
    git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@984 726aef4b-f618-498e-8847-2d620e286838

--- znc.cpp
+++ znc.cpp
@@ -1482,9 +1482,9 @@ bool CZNC::DoRehash(CString& sError)
 	// last step, throw unhandled config items at global config
 	for( u_long a = 0; a < vUnHandledLines.size(); a++ )
 	{
-		if( pChan == vUnHandledLines[a].m_pChan || pUser == vUnHandledLines[a].m_pUser )
+		if( ( pChan && pChan == vUnHandledLines[a].m_pChan ) || ( pUser && pUser == vUnHandledLines[a].m_pUser ) )
 			continue; // skip unclosed user or chan
-		if( GetModules().OnConfigLine( vUnHandledLines[a].m_sName, vUnHandledLines[a].m_sValue, vUnHandledLines[a].m_pUser, vUnHandledLines[a].m_pChan ) )
+		if( !GetModules().OnConfigLine( vUnHandledLines[a].m_sName, vUnHandledLines[a].m_sValue, vUnHandledLines[a].m_pUser, vUnHandledLines[a].m_pChan ) )
 		{
 			CUtils::PrintMessage( "Unhandled config line [" + vUnHandledLines[a].m_sName + "] = [" + vUnHandledLines[a].m_sValue + "]" );
 		}
