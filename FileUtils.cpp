znc/znc

commit e6007747d76477be2e22a008f3b7259091c9812f

Handle read errors in CFile::Copy()
This fixes an endless loop (However, I don't know how it happened).


git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1280 726aef4b-f618-498e-8847-2d620e286838

FileUtils.cpp
@@ -200,9 +200,19 @@ bool CFile::Copy(const CString& sOldFileName, const CString& sNewFileName, bool 
 	}
 
 	char szBuf[8192];
-	unsigned int len = 0;
+	int len = 0;
 
 	while ((len = OldFile.Read(szBuf, 8192))) {
+		if (len < 0) {
+			DEBUG_ONLY(cout << "CFile::Copy() failed: " << strerror(errno);)
+			OldFile.Close();
+
+			// That file is only a partial copy, get rid of it
+			NewFile.Close();
+			NewFile.Delete();
+
+			return false;
+		}
 		NewFile.Write(szBuf, len);
 	}
 

commit 9bec4b8048f65006ddf85aac825c4948e44363ae

Fix a bug introduced in r1280
git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1287 726aef4b-f618-498e-8847-2d620e286838

FileUtils.cpp
@@ -204,7 +204,7 @@ bool CFile::Copy(const CString& sOldFileName, const CString& sNewFileName, bool 
 
 	while ((len = OldFile.Read(szBuf, 8192))) {
 		if (len < 0) {
-			DEBUG_ONLY(cout << "CFile::Copy() failed: " << strerror(errno);)
+			DEBUG_ONLY(cout << "CFile::Copy() failed: " << strerror(errno) << endl);
 			OldFile.Close();
 
 			// That file is only a partial copy, get rid of it