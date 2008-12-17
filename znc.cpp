znc/znc

commit 4b1fbab6ebccb16c7b3e03174cd224fbfaab0c67

Remove CZNC::GetConfPath() and rework GetModPath() and GetUserPath()
This removes some unneeded variables.
Idea by kroimon.


git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1289 726aef4b-f618-498e-8847-2d620e286838

znc.cpp
@@ -438,11 +438,25 @@ void CZNC::InitDirs(const CString& sArgvPath, const CString& sDataDir) {
 	} else {
 		m_sZNCPath = sDataDir;
 	}
+}
 
-	// Other dirs that we use
-	m_sConfPath = m_sZNCPath + "/configs";
-	m_sModPath = m_sZNCPath + "/modules";
-	m_sUserPath = m_sZNCPath + "/users";
+CString CZNC::GetUserPath() const {
+	CString sUserPath = m_sZNCPath + "/users/";
+	if (!CFile::Exists(sUserPath)) {
+		CDir::MakeDir(sUserPath);
+	}
+
+	return sUserPath;
+}
+
+CString CZNC::GetModPath() const {
+	CString sModPath = m_sZNCPath + "/modules/";
+
+	if (!CFile::Exists(sModPath)) {
+		CDir::MakeDir(sModPath);
+	}
+
+	return sModPath;
 }
 
 
@@ -450,12 +464,12 @@ CString CZNC::ExpandConfigPath(const CString& sConfigFile) {
 	CString sRetPath;
 
 	if (sConfigFile.empty()) {
-		sRetPath = GetConfPath() + "/znc.conf";
+		sRetPath = GetZNCPath() + "/configs/znc.conf";
 	} else {
 		if (sConfigFile.Left(2) == "./" || sConfigFile.Left(3) == "../") {
 			sRetPath = GetCurPath() + "/" + sConfigFile;
 		} else if (sConfigFile.Left(1) != "/") {
-			sRetPath = GetConfPath() + "/" + sConfigFile;
+			sRetPath = GetZNCPath() + "/configs/" + sConfigFile;
 		} else {
 			sRetPath = sConfigFile;
 		}

znc.h
@@ -146,11 +146,10 @@ public:
 #endif
 	const CString& GetStatusPrefix() const { return m_sStatusPrefix; }
 	const CString& GetCurPath() const { if (!CFile::Exists(m_sCurPath)) { CDir::MakeDir(m_sCurPath); } return m_sCurPath; }
-	const CString& GetModPath() const { if (!CFile::Exists(m_sModPath)) { CDir::MakeDir(m_sModPath); } return m_sModPath; }
 	const CString& GetHomePath() const { if (!CFile::Exists(m_sHomePath)) { CDir::MakeDir(m_sHomePath); } return m_sHomePath; }
 	const CString& GetZNCPath() const { if (!CFile::Exists(m_sZNCPath)) { CDir::MakeDir(m_sZNCPath); } return m_sZNCPath; }
-	const CString& GetConfPath() const { if (!CFile::Exists(m_sConfPath)) { CDir::MakeDir(m_sConfPath); } return m_sConfPath; }
-	const CString& GetUserPath() const { if (!CFile::Exists(m_sUserPath)) { CDir::MakeDir(m_sUserPath); } return m_sUserPath; }
+	CString GetUserPath() const;
+	CString GetModPath() const;
 	CString GetPemLocation() const { return GetZNCPath() + "/znc.pem"; }
 	const CString& GetConfigFile() const { return m_sConfigFile; }
 	bool WritePemFile(bool bEncPem = false);
@@ -198,11 +197,8 @@ protected:
 	CSockManager			m_Manager;
 
 	CString					m_sCurPath;
-	CString					m_sModPath;
 	CString					m_sHomePath;
 	CString					m_sZNCPath;
-	CString					m_sConfPath;
-	CString					m_sUserPath;
 
 	CString					m_sConfigFile;
 	CString					m_sStatusPrefix;

commit 804b259f69b5e4fd7b5e3eeb138517e20b66b713

Fix a bug introduced in r1289
If the config dir (<datadir>/configs) didn't exist yet, ZNC didn't bother
creating it and thus failed to do write a config.

Thanks to m4v for testing a nightly and for reporting this bug.


git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1292 726aef4b-f618-498e-8847-2d620e286838

znc.cpp
@@ -440,6 +440,15 @@ void CZNC::InitDirs(const CString& sArgvPath, const CString& sDataDir) {
 	}
 }
 
+CString CZNC::GetConfPath() const {
+	CString sConfPath = m_sZNCPath + "/configs/";
+	if (!CFile::Exists(sConfPath)) {
+		CDir::MakeDir(sConfPath);
+	}
+
+	return sConfPath;
+}
+
 CString CZNC::GetUserPath() const {
 	CString sUserPath = m_sZNCPath + "/users/";
 	if (!CFile::Exists(sUserPath)) {
@@ -464,12 +473,12 @@ CString CZNC::ExpandConfigPath(const CString& sConfigFile) {
 	CString sRetPath;
 
 	if (sConfigFile.empty()) {
-		sRetPath = GetZNCPath() + "/configs/znc.conf";
+		sRetPath = GetConfPath() + "/znc.conf";
 	} else {
 		if (sConfigFile.Left(2) == "./" || sConfigFile.Left(3) == "../") {
 			sRetPath = GetCurPath() + "/" + sConfigFile;
 		} else if (sConfigFile.Left(1) != "/") {
-			sRetPath = GetZNCPath() + "/configs/" + sConfigFile;
+			sRetPath = GetConfPath() + "/" + sConfigFile;
 		} else {
 			sRetPath = sConfigFile;
 		}

znc.h
@@ -148,6 +148,7 @@ public:
 	const CString& GetCurPath() const { if (!CFile::Exists(m_sCurPath)) { CDir::MakeDir(m_sCurPath); } return m_sCurPath; }
 	const CString& GetHomePath() const { if (!CFile::Exists(m_sHomePath)) { CDir::MakeDir(m_sHomePath); } return m_sHomePath; }
 	const CString& GetZNCPath() const { if (!CFile::Exists(m_sZNCPath)) { CDir::MakeDir(m_sZNCPath); } return m_sZNCPath; }
+	CString GetConfPath() const;
 	CString GetUserPath() const;
 	CString GetModPath() const;
 	CString GetPemLocation() const { return GetZNCPath() + "/znc.pem"; }