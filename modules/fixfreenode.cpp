849c41a49bfcb31861d452af50aca13e5dfe8779
psychon <psychon@726aef4b-f618-498e-8847-2d620e286838>
2009-08-07 19:28:12 +0000

    Import the fixfreenode module from znc-extra
    Thanks to tomaw for the additional identify-ctcp "support"
    
    
    git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1591 726aef4b-f618-498e-8847-2d620e286838

---
+++ modules/fixfreenode.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2004-2009  See the AUTHORS file for details.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ */
+
+#include "Modules.h"
+
+class CPreventIdMsgMod : public CModule {
+public:
+	MODCONSTRUCTOR(CPreventIdMsgMod) {}
+
+	virtual EModRet OnUserRaw(CString& sLine) {
+		if (sLine.Token(0).AsLower() == "capab") {
+			if (sLine.AsLower().find("identify-msg") != CString::npos
+					|| sLine.AsLower().find("identify-ctcp") != CString::npos)
+				return HALTCORE;
+		}
+		return CONTINUE;
+	}
+};
+
+MODULEDEFS(CPreventIdMsgMod, "Prevent client from sending IDENTIFY-MSG to server")
