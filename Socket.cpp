c0793c6f31cfb13c6764bd7a7b834782a6b51d43
prozacx <prozacx@726aef4b-f618-498e-8847-2d620e286838>
2010-02-05 02:01:00 +0000

    Moved CSocket to Socket.cpp/h
    git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1733 726aef4b-f618-498e-8847-2d620e286838

--- Socket.cpp
+++ Socket.cpp
@@ -7,6 +7,9 @@
  */
 
 #include "Socket.h"
+#include "Modules.h"
+#include "User.h"
+#include "znc.h"
 
 unsigned int CSockManager::GetAnonConnectionCount(const CString &sIP) const {
 	const_iterator it;
@@ -24,3 +27,106 @@ unsigned int CSockManager::GetAnonConnectionCount(const CString &sIP) const {
 
 	return ret;
 }
+
+/////////////////// CSocket ///////////////////
+CSocket::CSocket(CModule* pModule) : CZNCSock() {
+	m_pModule = pModule;
+	m_pModule->AddSocket(this);
+	EnableReadLine();
+	SetMaxBufferThreshold(10240);
+}
+
+CSocket::CSocket(CModule* pModule, const CString& sHostname, unsigned short uPort, int iTimeout) : CZNCSock(sHostname, uPort, iTimeout) {
+	m_pModule = pModule;
+	m_pModule->AddSocket(this);
+	EnableReadLine();
+	SetMaxBufferThreshold(10240);
+}
+
+CSocket::~CSocket() {
+	CUser *pUser = m_pModule->GetUser();
+
+	m_pModule->UnlinkSocket(this);
+
+	if (!m_pModule->IsGlobal() && pUser) {
+		pUser->AddBytesWritten(GetBytesWritten());
+		pUser->AddBytesRead(GetBytesRead());
+	} else {
+		CZNC::Get().AddBytesWritten(GetBytesWritten());
+		CZNC::Get().AddBytesRead(GetBytesRead());
+	}
+}
+
+void CSocket::ReachedMaxBuffer() {
+	DEBUG(GetSockName() << " == ReachedMaxBuffer()");
+	PutModule("Some socket reached its max buffer limit and was closed!");
+	Close();
+}
+
+void CSocket::SockError(int iErrno) {
+	DEBUG(GetSockName() << " == SockError(" << strerror(iErrno) << ")");
+	if (iErrno == EMFILE) {
+		// We have too many open fds, this can cause a busy loop.
+		Close();
+	}
+}
+
+bool CSocket::ConnectionFrom(const CString& sHost, unsigned short uPort) {
+	return CZNC::Get().AllowConnectionFrom(sHost);
+}
+
+bool CSocket::Connect(const CString& sHostname, unsigned short uPort, bool bSSL, unsigned int uTimeout) {
+	CUser* pUser = m_pModule->GetUser();
+	CString sSockName = "MOD::C::" + m_pModule->GetModName();
+	CString sVHost;
+
+	if (pUser) {
+		sSockName += "::" + pUser->GetUserName();
+		sVHost = m_pModule->GetUser()->GetVHost();
+	}
+
+	// Don't overwrite the socket name if one is already set
+	if (!GetSockName().empty()) {
+		sSockName = GetSockName();
+	}
+
+	return m_pModule->GetManager()->Connect(sHostname, uPort, sSockName, uTimeout, bSSL, sVHost, this);
+}
+
+bool CSocket::Listen(unsigned short uPort, bool bSSL, unsigned int uTimeout) {
+	CUser* pUser = m_pModule->GetUser();
+	CString sSockName = "MOD::L::" + m_pModule->GetModName();
+
+	if (pUser) {
+		sSockName += "::" + pUser->GetUserName();
+	}
+	// Don't overwrite the socket name if one is already set
+	if (!GetSockName().empty()) {
+		sSockName = GetSockName();
+	}
+
+	return m_pModule->GetManager()->ListenAll(uPort, sSockName, bSSL, SOMAXCONN, this);
+}
+
+bool CSocket::PutIRC(const CString& sLine) {
+	return (m_pModule) ? m_pModule->PutIRC(sLine) : false;
+}
+
+bool CSocket::PutUser(const CString& sLine) {
+	return (m_pModule) ? m_pModule->PutUser(sLine) : false;
+}
+
+bool CSocket::PutStatus(const CString& sLine) {
+	return (m_pModule) ? m_pModule->PutStatus(sLine) : false;
+}
+
+bool CSocket::PutModule(const CString& sLine, const CString& sIdent, const CString& sHost) {
+	return (m_pModule) ? m_pModule->PutModule(sLine, sIdent, sHost) : false;
+}
+bool CSocket::PutModNotice(const CString& sLine, const CString& sIdent, const CString& sHost) {
+	return (m_pModule) ? m_pModule->PutModNotice(sLine, sIdent, sHost) : false;
+}
+
+void CSocket::SetModule(CModule* p) { m_pModule = p; }
+CModule* CSocket::GetModule() const { return m_pModule; }
+/////////////////// !CSocket ///////////////////
