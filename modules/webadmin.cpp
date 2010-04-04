f8811a18973a73552c930ea8d6e8402a109027a2
cflakes <cflakes@726aef4b-f618-498e-8847-2d620e286838>
2010-04-04 11:56:10 +0000

    WebAdmin: Show global settings previous to user settings.
    git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1870 726aef4b-f618-498e-8847-2d620e286838

--- modules/webadmin.cpp
+++ modules/webadmin.cpp
@@ -35,9 +35,8 @@ public:
 	GLOBALMODCONSTRUCTOR(CWebAdminMod) {
 		VPair vParams;
 		vParams.push_back(make_pair("user", ""));
-		AddSubPage(new CWebSubPage("edituser", "Your Settings", vParams));
-
 		AddSubPage(new CWebSubPage("settings", "Global Settings", CWebSubPage::F_ADMIN));
+		AddSubPage(new CWebSubPage("edituser", "Your Settings", vParams));
 		AddSubPage(new CWebSubPage("listusers", "List Users", CWebSubPage::F_ADMIN));
 		AddSubPage(new CWebSubPage("adduser", "Add User", CWebSubPage::F_ADMIN));
 	}
