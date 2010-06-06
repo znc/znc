501c76a4d50f1c8e7c6f4795301f9bca5f4793b7
psychon <psychon@726aef4b-f618-498e-8847-2d620e286838>
2010-06-06 07:23:52 +0000

    Increase the version number to 0.090
    git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@2007 726aef4b-f618-498e-8847-2d620e286838

--- main.h
+++ main.h
@@ -11,7 +11,7 @@
 
 // The following defines are for #if comparison (preprocessor only likes ints)
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  89
+#define VERSION_MINOR  90
 // This one is for display purpose
 #define VERSION        (VERSION_MAJOR + VERSION_MINOR / 1000.0)
 
