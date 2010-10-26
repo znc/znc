znc/znc

commit a0a63d5bc45c6e17f7b5c91b5ef10ff467dc5596

Remove --disable-modules
ZNC without modules is like rain without being inside,
you really don't want that!


git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@1794 726aef4b-f618-498e-8847-2d620e286838

configure.in
@@ -77,10 +77,6 @@ AC_ARG_ENABLE( [ipv6],
 	AS_HELP_STRING([--disable-ipv6], [disable ipv6 support]),
 	[IPV6="$enableval"],
 	[IPV6="yes"])
-AC_ARG_ENABLE( [modules],
-	AS_HELP_STRING([--disable-modules], [disable modules]),
-	[MODULES="$enableval"],
-	[MODULES="yes"])
 AC_ARG_ENABLE( [openssl],
 	AS_HELP_STRING([--disable-openssl], [disable openssl]),
 	[if test "$enableval" = "no" ; then NOSSL=1; fi],)
@@ -177,7 +173,10 @@ AC_ARG_WITH( [module-data-prefix],
 	[DATADIR="${datadir}/znc"] )
 
 
-if test "$MODULES" = "yes"; then
+# This used to be
+# if test "$MODULES" = "yes"; then
+# but I'm too lazy to reindent all of this
+if test "yes" = "yes"; then
 	FOUND_DL=no
 
 	# Don't use any libs like -lssl -lcrypto when checking for dlopen(),
@@ -194,14 +193,12 @@ if test "$MODULES" = "yes"; then
 		AC_MSG_ERROR([could not find dlopen. Try --disable-modules])
 	fi
 
-	appendCXX -D_MODULES
 	appendMod "$CXXFLAGS"
 
 	if test -z "$ISSUN" -a -z "$ISDARWIN"; then
 		# This is an unknown compiler flag on some OS
 		appendLD -Wl,--export-dynamic
 	fi
-	MODTARGET="modules"
 
 	if test -z "$ISCYGWIN" ; then
 		# cygwin doesn't need -fPIC, everything else does (for modules)
@@ -297,7 +294,6 @@ AC_SUBST([LIBS])
 AC_SUBST([LIBZNC])
 AC_SUBST([LIBZNCDIR])
 AC_SUBST([MODLINK])
-AC_SUBST([MODTARGET])
 AC_SUBST([NOSSL])
 AC_SUBST([EXTRA])
 AC_SUBST([PERL])
@@ -311,9 +307,7 @@ AC_CONFIG_FILES([znc-buildmod])
 AC_CONFIG_FILES([man/Makefile])
 AC_CONFIG_FILES([znc.pc])
 AC_CONFIG_FILES([znc-uninstalled.pc])
-if test "$MODULES" = "yes"; then
-	AC_CONFIG_FILES([modules/Makefile])
-fi
+AC_CONFIG_FILES([modules/Makefile])
 AC_OUTPUT
 
 echo
@@ -327,7 +321,6 @@ else
 	echo "openssl: yes"
 fi
 echo "c-ares:  $ARES"
-echo "modules: $MODULES"
 if test x"$PERL" = "x" ; then
 	echo "perl:    no"
 else

Chan.cpp
@@ -215,13 +215,11 @@ void CChan::ModeChange(const CString& sModes, const CString& sOpNick) {
 	CString sArgs = sModes.Token(1, true);
 	bool bAdd = true;
 
-#ifdef _MODULES
 	CNick* pOpNick = FindNick(sOpNick);
 
 	if (pOpNick) {
 		MODULECALL(OnRawMode(*pOpNick, *this, sModeArg, sArgs), m_pUser, NULL, );
 	}
-#endif
 
 	for (unsigned int a = 0; a < sModeArg.size(); a++) {
 		const unsigned char& uMode = sModeArg[a];
@@ -250,7 +248,6 @@ void CChan::ModeChange(const CString& sModes, const CString& sOpNick) {
 							RemPerm(uPerm);
 						}
 					}
-#ifdef _MODULES
 					bool bNoChange = (pNick->HasPerm(uPerm) == bAdd);
 
 					if (uMode && pOpNick) {
@@ -270,7 +267,6 @@ void CChan::ModeChange(const CString& sModes, const CString& sOpNick) {
 							}
 						}
 					}
-#endif
 				}
 			}
 		} else {
@@ -295,7 +291,6 @@ void CChan::ModeChange(const CString& sModes, const CString& sOpNick) {
 					break;
 			}
 
-#ifdef _MODULES
 			bool bNoChange;
 			if (bList) {
 				bNoChange = false;
@@ -305,7 +300,6 @@ void CChan::ModeChange(const CString& sModes, const CString& sOpNick) {
 				bNoChange = !HasMode(uMode);
 			}
 			MODULECALL(OnMode(*pOpNick, *this, uMode, sArg, bAdd, bNoChange), m_pUser, NULL, );
-#endif
 
 			if (!bList) {
 				(bAdd) ? AddMode(uMode, sArg) : RemMode(uMode);

Client.cpp
@@ -64,7 +64,6 @@ void CClient::ReadLine(const CString& sData) {
 
 	DEBUG("(" << ((m_pUser) ? m_pUser->GetUserName() : GetRemoteIP()) << ") CLI -> ZNC [" << sLine << "]");
 
-#ifdef _MODULES
 	if (IsAttached()) {
 		MODULECALL(OnUserRaw(sLine), m_pUser, this, return);
 	} else {
@@ -91,7 +90,6 @@ void CClient::ReadLine(const CString& sData) {
 			return;
 		}
 	}
-#endif
 
 	CString sCommand = sLine.Token(0);
 	if (sCommand.Left(1) == ":") {
@@ -176,12 +174,10 @@ void CClient::ReadLine(const CString& sData) {
 			else
 				UserCommand(sModCommand);
 		} else {
-#ifdef _MODULES
 			if (sModCommand.empty())
 				CALLMOD(sTarget, this, m_pUser, PutModule("Hello. How may I help you?"))
 			else
 				CALLMOD(sTarget, this, m_pUser, OnModCommand(sModCommand))
-#endif
 		}
 		return;
 	} else if (sCommand.Equals("DETACH")) {
@@ -332,11 +328,9 @@ void CClient::ReadLine(const CString& sData) {
 		}
 
 		if (sTarget.TrimPrefix(m_pUser->GetStatusPrefix())) {
-#ifdef _MODULES
 			if (!sTarget.Equals("status")) {
 				CALLMOD(sTarget, this, m_pUser, OnModNotice(sMsg));
 			}
-#endif
 			return;
 		}
 
@@ -348,7 +342,6 @@ void CClient::ReadLine(const CString& sData) {
 		}
 #endif
 
-#ifdef _MODULES
 		if (sMsg.WildCmp("\001*\001")) {
 			CString sCTCP = sMsg;
 			sCTCP.LeftChomp();
@@ -360,7 +353,6 @@ void CClient::ReadLine(const CString& sData) {
 		} else {
 			MODULECALL(OnUserNotice(sTarget, sMsg), m_pUser, this, return);
 		}
-#endif
 
 		if (!GetIRCSock()) {
 			// Some lagmeters do a NOTICE to their own nick, ignore those.
@@ -498,9 +490,7 @@ void CClient::ReadLine(const CString& sData) {
 				if (sTarget.Equals("status")) {
 					StatusCTCP(sCTCP);
 				} else {
-#ifdef _MODULES
 					CALLMOD(sTarget, this, m_pUser, OnModCTCP(sCTCP));
-#endif
 				}
 				return;
 			}
@@ -540,9 +530,7 @@ void CClient::ReadLine(const CString& sData) {
 			if (sTarget.Equals("status")) {
 				UserCommand(sMsg);
 			} else {
-#ifdef _MODULES
 				CALLMOD(sTarget, this, m_pUser, OnModCommand(sMsg));
-#endif
 			}
 			return;
 		}
@@ -622,26 +610,6 @@ bool CClient::SendMotd() {
 }
 
 void CClient::AuthUser() {
-	/*
-#ifdef _MODULES
-	if (CZNC::Get().GetModules().OnLoginAttempt(m_sUser, m_sPass, *this)) {
-		return;
-	}
-#endif
-
-	CUser* pUser = CZNC::Get().GetUser(m_sUser);
-
-	if (pUser && pUser->CheckPass(m_sPass)) {
-		AcceptLogin(*pUser);
-	} else {
-		if (pUser) {
-			pUser->PutStatus("Another client attempted to login as you, with a bad password.");
-		}
-
-		RefuseLogin();
-	}
-	*/
-
 	m_spAuth = new CClientAuth(this, m_sUser, m_sPass);
 
 	CZNC::Get().AuthUser(m_spAuth);
@@ -689,9 +657,7 @@ void CAuthBase::RefuseLogin(const CString& sReason) {
 				"to login as you, but was rejected [" + sReason + "].");
 	}
 
-#ifdef _MODULES
 	CZNC::Get().GetModules().OnFailedLogin(GetUsername(), GetRemoteIP());
-#endif
 	RefusedLogin(sReason);
 	Invalidate();
 }

ClientCommand.cpp
@@ -547,7 +547,6 @@ void CClient::UserCommand(CString& sLine) {
 			PutStatus("You have no active DCCs.");
 		}
 	} else if (sCommand.Equals("LISTMODS") || sCommand.Equals("LISTMODULES")) {
-#ifdef _MODULES
 		if (m_pUser->IsAdmin()) {
 			CModules& GModules = CZNC::Get().GetModules();
 
@@ -587,12 +586,8 @@ void CClient::UserCommand(CString& sLine) {
 
 			PutStatus(Table);
 		}
-#else
-		PutStatus("Modules are not enabled.");
-#endif
 		return;
 	} else if (sCommand.Equals("LISTAVAILMODS") || sCommand.Equals("LISTAVAILABLEMODULES")) {
-#ifdef _MODULES
 		if (m_pUser->DenyLoadMod()) {
 			PutStatus("Access Denied.");
 			return;
@@ -643,9 +638,6 @@ void CClient::UserCommand(CString& sLine) {
 
 			PutStatus(Table);
 		}
-#else
-		PutStatus("Modules are not enabled.");
-#endif
 		return;
 	} else if (sCommand.Equals("LOADMOD") || sCommand.Equals("LOADMODULE")) {
 		CString sMod;
@@ -664,7 +656,6 @@ void CClient::UserCommand(CString& sLine) {
 			return;
 		}
 
-#ifdef _MODULES
 		CModInfo ModInfo;
 		CString sRetMsg;
 		if (!CZNC::Get().GetModules().GetModInfo(ModInfo, sMod, sRetMsg)) {
@@ -694,9 +685,6 @@ void CClient::UserCommand(CString& sLine) {
 		}
 
 		PutStatus(sModRet);
-#else
-		PutStatus("Unable to load [" + sMod + "] Modules are not enabled.");
-#endif
 		return;
 	} else if (sCommand.Equals("UNLOADMOD") || sCommand.Equals("UNLOADMODULE")) {
 		CString sMod;
@@ -706,7 +694,6 @@ void CClient::UserCommand(CString& sLine) {
 			PutStatus("Unable to unload [" + sMod + "] Access Denied.");
 			return;
 		}
-#ifdef _MODULES
 		if (sMod.empty()) {
 			PutStatus("Usage: UnloadMod <module>");
 			return;
@@ -723,9 +710,6 @@ void CClient::UserCommand(CString& sLine) {
 		}
 
 		PutStatus(sModRet);
-#else
-		PutStatus("Unable to unload [" + sMod + "] Modules are not enabled.");
-#endif
 		return;
 	} else if (sCommand.Equals("RELOADMOD") || sCommand.Equals("RELOADMODULE")) {
 		CString sMod;
@@ -744,7 +728,6 @@ void CClient::UserCommand(CString& sLine) {
 			return;
 		}
 
-#ifdef _MODULES
 		CModInfo ModInfo;
 		CString sRetMsg;
 		if (!CZNC::Get().GetModules().GetModInfo(ModInfo, sMod, sRetMsg)) {
@@ -768,14 +751,8 @@ void CClient::UserCommand(CString& sLine) {
 		}
 
 		PutStatus(sModRet);
-#else
-		PutStatus("Unable to unload [" + sMod + "] Modules are not enabled.");
-#endif
 		return;
 	} else if ((sCommand.Equals("UPDATEMOD") || sCommand.Equals("UPDATEMODULE")) && m_pUser->IsAdmin() ) {
-#ifndef _MODULES
-		PutStatus("Modules are not enabled.");
-#else
 		CString sMod = sLine.Token(1);
 
 		if (sMod.empty()) {
@@ -795,7 +772,6 @@ void CClient::UserCommand(CString& sLine) {
 			PutStatus("Done, but there were errors, some users no longer have ["
 					+ sMod + "] loaded");
 		}
-#endif
 	} else if (sCommand.Equals("ADDVHOST") && m_pUser->IsAdmin()) {
 		CString sVHost = sLine.Token(1);
 

HTTPSock.cpp
@@ -6,8 +6,6 @@
  * by the Free Software Foundation.
  */
 
-#ifdef _MODULES
-
 #include "Modules.h"
 #include "HTTPSock.h"
 #include "znc.h"
@@ -459,5 +457,3 @@ void CHTTPSock::ReachedMaxBuffer() {
 	DEBUG(GetSockName() << " == ReachedMaxBuffer()");
 	Close();
 }
-
-#endif // _MODULES

HTTPSock.h
@@ -6,8 +6,6 @@
  * by the Free Software Foundation.
  */
 
-#ifdef _MODULES
-
 #ifndef _HTTPSOCK_H
 #define _HTTPSOCK_H
 
@@ -94,5 +92,3 @@ protected:
 };
 
 #endif // !_HTTPSOCK_H
-
-#endif // _MODULES

Makefile.in
@@ -32,7 +32,7 @@ DISTCLEAN := Makefile config.log config.status znc-config znc-buildmod .depend \
 
 .PHONY: all man modules clean distclean install
 
-all: znc man @MODTARGET@ $(LIBZNC)
+all: znc man modules $(LIBZNC)
 
 ifeq "$(LIBZNC)" ""
 OBJS := $(BIN_OBJS) $(LIB_OBJS)
@@ -51,17 +51,11 @@ man:
 	@$(MAKE) -C man
 
 modules: $(LIBZNC)
-	@if test -n "@MODTARGET@"; then \
-		$(MAKE) -C modules; \
-	else \
-		echo "Modules are not enabled"; \
-	fi
+	$(MAKE) -C modules;
 
 clean:
 	rm -rf $(CLEAN)
-	@if test -n "@MODTARGET@"; then \
-		$(MAKE) -C modules clean; \
-	fi
+	@$(MAKE) -C modules clean;
 	@$(MAKE) -C man clean
 
 distclean: clean
@@ -82,9 +76,7 @@ install: znc $(LIBZNC)
 	install -m 0755 znc-buildmod $(DESTDIR)$(bindir)
 	install -m 0644 $(srcdir)/*.h $(DESTDIR)$(includedir)/znc
 	install -m 0644 znc.pc $(DESTDIR)$(PKGCONFIGDIR)
-	@if test -n "@MODTARGET@"; then \
-		$(MAKE) -C modules install DESTDIR=$(DESTDIR); \
-	fi
+	@$(MAKE) -C modules install DESTDIR=$(DESTDIR);
 	if test -n "$(LIBZNC)"; then \
 		mkdir -p $(DESTDIR)$(LIBZNCDIR) || exit 1 ; \
 		install -m 0755 $(LIBZNC) $(DESTDIR)$(LIBZNCDIR) || exit 1 ; \

Modules.cpp
@@ -6,8 +6,6 @@
  * by the Free Software Foundation.
  */
 
-#ifdef _MODULES
-
 #include "Modules.h"
 #include "User.h"
 #include "znc.h"
@@ -990,5 +988,3 @@ ModHandle CModules::OpenModule(const CString& sModule, const CString& sModPath, 
 
 	return p;
 }
-
-#endif // _MODULES

commit 3c1b5d8f702344c1fea8c56fe56ce8a554c89fef

Fix bug in UI that was introduced in r1794.
ZNC shouldn't tell people to use unexistant options to ./configure

git-svn-id: https://znc.svn.sourceforge.net/svnroot/znc/trunk@2166 726aef4b-f618-498e-8847-2d620e286838

aclocal.m4
@@ -148,7 +148,7 @@ $$1_PKG_ERRORS
 Consider adjusting the PKG_CONFIG_PATH environment variable if you
 installed software in a non-standard prefix.
 
-_PKG_TEXT])dnl
+_PKG_TEXT])[]dnl
         ])
 elif test $pkg_failed = untried; then
      	AC_MSG_RESULT([no])
@@ -159,7 +159,7 @@ path to pkg-config.
 
 _PKG_TEXT
 
-To get pkg-config, see <http://pkg-config.freedesktop.org/>.])dnl
+To get pkg-config, see <http://pkg-config.freedesktop.org/>.])[]dnl
         ])
 else
 	$1[]_CFLAGS=$pkg_cv_[]$1[]_CFLAGS

configure
@@ -3219,7 +3219,7 @@ fi
 appendLib "$SAVED_LIBS"
 
 if test $FOUND_DL = "no" ; then
-	as_fn_error $? "Could not find dlopen. Try --disable-modules" "$LINENO" 5
+	as_fn_error $? "Could not find dlopen. ZNC will not work on this box until you upgrade this ancient system or at least install needed system libraries." "$LINENO" 5
 fi
 
 # ----- Check for c-ares

configure.in
@@ -167,7 +167,7 @@ AC_CHECK_LIB( dl, dlopen, FOUND_DL=yes ; appendLib -ldl,)
 appendLib "$SAVED_LIBS"
 
 if test $FOUND_DL = "no" ; then
-	AC_MSG_ERROR([Could not find dlopen. Try --disable-modules])
+	AC_MSG_ERROR([Could not find dlopen. ZNC will not work on this box until you upgrade this ancient system or at least install needed system libraries.])
 fi
 
 # ----- Check for c-ares