080bf674e011a8d7205368fceeaab11fbc70c71b
Alexey Sokolov <somebody@desktop.darthgandalf.co.cc>
2011-02-19 21:37:03 +0600

    Add perl module perleval.
    It allows you to evaluate perl code you /msg to it.

---
+++ modules/perleval.pm
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+
+package perleval;
+use base 'ZNC::Module';
+
+sub description {
+	'Evaluates perl code'
+}
+
+sub OnLoad {
+	my $self = shift;
+	if (!$self->GetUser->IsAdmin) {
+		$_[1] = 'Only admin can load this module';
+		return 0
+	}
+	return 1
+}
+
+sub OnModCommand {
+	my $self = shift;
+	my $cmd = shift;
+	my $x = eval $cmd;
+	if ($@) {
+		$self->PutModule("Error: $@")
+	} else {
+		$self->PutModule("Result: $x")
+	}
+}
+
+1
