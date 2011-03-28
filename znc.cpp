znc/znc

commit d735e9d881f58a38b37f276ce2adee2e90a0ef46

Remove some pointless if statements

znc.cpp
@@ -348,9 +348,7 @@ bool CZNC::DeletePidFile() {
 
 	CUtils::PrintAction("Deleting pid file [" + File->GetLongName() + "]");
 
-	bool bRet = false;
-	if (File->Delete())
-		bRet = true;
+	bool bRet = File->Delete();
 
 	delete File;
 	CUtils::PrintStatus(bRet);
@@ -416,9 +414,7 @@ bool CZNC::IsHostAllowed(const CString& sHostMask) const {
 bool CZNC::AllowConnectionFrom(const CString& sIP) const {
 	if (m_uiAnonIPLimit == 0)
 		return true;
-	if (GetManager().GetAnonConnectionCount(sIP) >= m_uiAnonIPLimit)
-		return false;
-	return true;
+	return (GetManager().GetAnonConnectionCount(sIP) < m_uiAnonIPLimit)
 }
 
 void CZNC::InitDirs(const CString& sArgvPath, const CString& sDataDir) {

FileUtils.cpp
@@ -259,11 +259,7 @@ bool CFile::Truncate() {
 }
 
 bool CFile::Sync() {
-	if (m_iFD != -1 && fsync(m_iFD) == 0) {
-		return true;
-	}
-
-	return false;
+	return (m_iFD != -1 && fsync(m_iFD) == 0);
 }
 
 bool CFile::Open(const CString& sFileName, int iFlags, mode_t iMode) {
@@ -414,11 +410,7 @@ bool CFile::Lock(int iType, bool bBlocking) {
 	fl.l_whence = SEEK_SET;
 	fl.l_start  = 0;
 	fl.l_len    = 0;
-	if (fcntl(m_iFD, (bBlocking ? F_SETLKW : F_SETLK), &fl) == -1) {
-		return false;
-	} else {
-		return true;
-	}
+	return (fcntl(m_iFD, (bBlocking ? F_SETLKW : F_SETLK), &fl) != -1);
 }
 
 bool CFile::IsOpen() const { return (m_iFD != -1); }

Server.cpp
@@ -18,15 +18,7 @@ CServer::CServer(const CString& sName, unsigned short uPort, const CString& sPas
 CServer::~CServer() {}
 
 bool CServer::IsValidHostName(const CString& sHostName) {
-	if (sHostName.empty()) {
-		return false;
-	}
-
-	if (sHostName.find(' ') != CString::npos) {
-		return false;
-	}
-
-	return true;
+	return (!sHostName.empty() && (sHostName.find(' ') == CString::npos));
 }
 
 const CString& CServer::GetName() const { return m_sName; }

User.cpp
@@ -168,14 +168,7 @@ void CUser::SetIRCSocket(CIRCSock* pIRCSock) {
 bool CUser::IsIRCConnected() const
 {
 	const CIRCSock* pSock = GetIRCSock();
-
-	if (!pSock)
-		return false;
-
-	if (!pSock->IsConnected())
-		return false;
-
-	return true;
+	return (pSock && pSock->IsConnected());
 }
 
 void CUser::IRCDisconnected() {
@@ -630,9 +623,7 @@ bool CUser::PrintLine(CFile& File, CString sName, CString sValue) const {
 	// FirstLine() so that no one can inject new lines to the config if he
 	// manages to add "\n" to e.g. sValue.
 	CString sLine = "\t" + sName.FirstLine() + " = " + sValue.FirstLine() + "\n";
-	if (File.Write(sLine) <= 0)
-		return false;
-	return true;
+	return (File.Write(sLine) > 0);
 }
 
 bool CUser::WriteConfig(CFile& File) {

main.cpp
@@ -69,10 +69,7 @@ static void signalHandler(int sig) {
 
 static bool isRoot() {
 	// User root? If one of these were root, we could switch the others to root, too
-	if (geteuid() == 0 || getuid() == 0)
-		return true;
-
-	return false;
+	return (geteuid() == 0 || getuid() == 0);
 }
 
 static void seedPRNG() {

modules/watch.cpp
@@ -84,10 +84,7 @@ public:
 			return false;
 		if (!Nick.GetHostMask().AsLower().WildCmp(m_sHostMask.AsLower()))
 			return false;
-		if (!sText.AsLower().WildCmp(pUser->ExpandString(m_sPattern).AsLower()))
-			return false;
-
-		return true;
+		return (sText.AsLower().WildCmp(pUser->ExpandString(m_sPattern).AsLower()));
 	}
 
 	bool operator ==(const CWatchEntry& WatchEntry) {

modules/webadmin.cpp
@@ -44,10 +44,7 @@ inline bool FOR_EACH_MODULE_CanContinue(FOR_EACH_MODULE_Type& state, CModules::i
 		i = state.CMuser.begin();
 		state.bOnCMuser = true;
 	}
-	if (state.bOnCMuser && i == state.CMuser.end()) {
-		return false;
-	}
-	return true;
+	return !(state.bOnCMuser && i == state.CMuser.end());
 }
 
 #define FOR_EACH_MODULE(I, pUser)\

commit 36ffa163a8c87ac83915e08c319856aa53ce8451

Make znc.cpp to be compilable again.
This was broken since d735e9d881f58a38b37f276ce

znc.cpp
@@ -414,7 +414,7 @@ bool CZNC::IsHostAllowed(const CString& sHostMask) const {
 bool CZNC::AllowConnectionFrom(const CString& sIP) const {
 	if (m_uiAnonIPLimit == 0)
 		return true;
-	return (GetManager().GetAnonConnectionCount(sIP) < m_uiAnonIPLimit)
+	return (GetManager().GetAnonConnectionCount(sIP) < m_uiAnonIPLimit);
 }
 
 void CZNC::InitDirs(const CString& sArgvPath, const CString& sDataDir) {