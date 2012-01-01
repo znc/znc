927c1d307486f3087b6e2cf2e2a9660bd9c4906e
Uli Schlachter <psychon@znc.in>
2012-01-01 13:48:13 +0100

    Increase the version number to 0.203
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- main.h
+++ main.h
@@ -13,7 +13,7 @@
 
 // The following defines are for #if comparison (preprocessor only likes ints)
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  202
+#define VERSION_MINOR  203
 // This one is for display purpose
 #define VERSION        (VERSION_MAJOR + VERSION_MINOR / 1000.0)
 
