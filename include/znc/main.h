95053f4db48d881f8eefd2558d90b2851bfc2e89
Alexey Sokolov <alexey@asokolov.org>
2012-03-29 23:28:25 +0700

    Increase the version number to 0.207

--- include/znc/main.h
+++ include/znc/main.h
@@ -13,7 +13,7 @@
 
 // The following defines are for #if comparison (preprocessor only likes ints)
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  205
+#define VERSION_MINOR  207
 // This one is for display purpose
 #define VERSION        (VERSION_MAJOR + VERSION_MINOR / 1000.0)
 
