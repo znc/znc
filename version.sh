58a34fa5adea8ffc519f03b30ec9c2f377bab9cd
Uli Schlachter <psychon@znc.in>
2013-02-09 13:59:31 +0100

    Improve the way that VERSION_EXTRA is calculated
    Instead of doing magic with the result from version.sh so that we can add a
    preprocessor macro, the script now just writes src/version.cpp with the expected
    content. This should fix all the various issues that we have with quoting the
    arguments and things that go wrong when not building znc from a git clone.
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- version.sh
+++ version.sh
@@ -1,25 +1,39 @@
 #!/bin/sh
 
-# Change into the source directory
-cd `dirname $0`
+# Get the path to the source directory
+GIT_DIR=`dirname $0`
 
-# Our first argument should be the path to git
-GIT="$1"
+# Our first argument should be the file that we write to
+OUTPUT="$1"
+
+# Our second argument should be the path to git
+GIT="$2"
 if [ "x$GIT" = "x" ]
 then
-	# Let's hope the best
-	GIT=git
+	EXTRA=""
+else
+	GIT="${GIT} --git-dir=${GIT_DIR}/.git"
+
+	# Figure out the information we need
+	LATEST_TAG=`${GIT} describe --abbrev=0 HEAD`
+	COMMITS_SINCE=`${GIT} log --format=oneline ${LATEST_TAG}..HEAD | wc -l`
+	SHORT_ID=`${GIT} rev-parse --short HEAD`
+
+	# If this commit is tagged, don't print anything
+	# (the assumption here is: this is a release)
+	if [ "x$COMMITS_SINCE" = "x0" ]
+	then
+		EXTRA=""
+	else
+		EXTRA="-git-${COMMITS_SINCE}-${SHORT_ID}"
+	fi
 fi
 
-# Figure out the information we need
-LATEST_TAG=`${GIT} describe --abbrev=0 HEAD`
-COMMITS_SINCE=`${GIT} log --format=oneline ${LATEST_TAG}..HEAD | wc -l`
-SHORT_ID=`${GIT} rev-parse --short HEAD`
-
-# If this commit is tagged, don't print anything
-# (the assumption here is: this is a release)
-if [ "x$COMMITS_SINCE" = "x0" ]
+# Generate output file, if any
+if [ "x$OUTPUT" != "x" ]
 then
-	exit 0
+	echo '#include <znc/version.h>' > $OUTPUT
+	echo "const char* ZNC_VERSION_EXTRA = \"$EXTRA\";" >> $OUTPUT
 fi
-echo "-DVERSION_EXTRA=\\\"-git-${COMMITS_SINCE}-${SHORT_ID}\\\""
+
+echo "$EXTRA"
