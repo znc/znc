bd2c099a5080be51c6e80236d15fbcc812a5d5e0
dgw <dgw@technobabbl.es>
2013-12-17 16:38:18 -0500

    Add identify prompt for StarChat.net
    Apparently StarChat.net uses a very old (?) or obscure NickServ
    service that doesn't send any of the more common prompts for ID.
    (It also doesn't support nick grouping, more's the pity.)
    (cherry picked from commit e379a7a81a4559072c12ec02ccb43b1591cf0a0b)

--- modules/nickserv.cpp
+++ modules/nickserv.cpp
@@ -163,6 +163,7 @@ public:
 				 || sMessage.find("choose a different nickname") != CString::npos
 				 || sMessage.find("If this is your nick, identify yourself with") != CString::npos
 				 || sMessage.find("If this is your nick, type") != CString::npos
+				 || sMessage.find("This is a registered nickname, please identify") != CString::npos
 				 || sMessage.StripControls_n().find("type /NickServ IDENTIFY password") != CString::npos)
 				&& sMessage.AsUpper().find("IDENTIFY") != CString::npos
 				&& sMessage.find("help") == CString::npos) {
