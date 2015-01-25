26e466c677953313dce3ca98362025f9fa0b4cd2
Jos <buughost@gmail.com>
2015-01-25 10:25:13 +0100

    More detailed instructions on how to connect to ZNC when not yet logged in.
    The message now mentions you should configure your client to automatically log you in, and also lets you know how to connect to a specific network.

--- src/Client.cpp
+++ src/Client.cpp
@@ -82,7 +82,10 @@ void CClient::SendRequiredPasswordNotice() {
 	PutClient(":irc.znc.in 464 " + GetNick() + " :Password required");
 	PutClient(":irc.znc.in NOTICE AUTH :*** "
 			  "You need to send your password. "
-			  "Try /quote PASS <username>/<network>:<password>");
+			  "Configure your client to send a server password.");
+	PutClient(":irc.znc.in NOTICE AUTH :*** "
+			  "To connect now, you can use /quote PASS <username>:<password>, "
+			  "or /quote PASS <username>/<network>:<password> to connect to a specific network.");
 }
 
 void CClient::ReadLine(const CString& sData) {
