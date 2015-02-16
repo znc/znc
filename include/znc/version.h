2397f8842af97d629e21b0ae758cab92fefcdd89
J-P Nurmi <jpnurmi@gmail.com>
2015-02-16 20:22:34 +0100

    Bump the version to 1.7

--- include/znc/version.h
+++ include/znc/version.h
@@ -3,10 +3,10 @@
 
 // The following defines are for #if comparison (preprocessor only likes ints)
 #define VERSION_MAJOR  1
-#define VERSION_MINOR  6
+#define VERSION_MINOR  7
 #define VERSION_PATCH  0
 // This one is for display purpose
-#define VERSION_STR    "1.6.0"
+#define VERSION_STR    "1.7.0"
 // This one is for ZNCModInfo
 #define VERSION        (VERSION_MAJOR + VERSION_MINOR / 10.0)
 
