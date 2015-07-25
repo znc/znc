757ba2bee7302c49af9bae13b2a156bec52074a7
J-P Nurmi <jpnurmi@gmail.com>
2015-07-25 22:44:07 +0200

    ZNC 1.6.1-rc1

--- include/znc/version.h
+++ include/znc/version.h
@@ -4,9 +4,9 @@
 // The following defines are for #if comparison (preprocessor only likes ints)
 #define VERSION_MAJOR  1
 #define VERSION_MINOR  6
-#define VERSION_PATCH  -1
+#define VERSION_PATCH  1
 // This one is for display purpose
-#define VERSION_STR    "1.6.x"
+#define VERSION_STR    "1.6.1-rc1"
 // This one is for ZNCModInfo
 #define VERSION        (VERSION_MAJOR + VERSION_MINOR / 10.0)
 
