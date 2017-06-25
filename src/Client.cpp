0ce6b7c8ba23e52445f08a027fe444965e957134
Alexey Sokolov <alexey+znc@asokolov.org>
2017-06-26 00:28:57 +0100

    Fix a warning
    Reported by s7r

--- src/Client.cpp
+++ src/Client.cpp
@@ -888,13 +888,13 @@ void CClient::AddBuffer(const T& Message) {
 }
 
 void CClient::EchoMessage(const CMessage& Message) {
+    CMessage EchoedMessage = Message;
     for (CClient* pClient : GetClients()) {
         if (pClient->HasEchoMessage() ||
             (pClient != this && (m_pNetwork->IsChan(Message.GetParam(0)) ||
                                  pClient->HasSelfMessage()))) {
-            CMessage EchoMessage(Message);
-            EchoMessage.SetNick(GetNickMask());
-            pClient->PutClient(EchoMessage);
+            EchoedMessage.SetNick(GetNickMask());
+            pClient->PutClient(EchoedMessage);
         }
     }
 }
