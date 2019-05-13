d9322928df224497b2015be01d509bea10c0a94c
MalaGaM @ ARTiSPRETiS <malagam.artispretis@gmail.com>
2019-05-13 22:51:03 +0200

    Update modtcl.tcl

--- modules/modtcl/modtcl.tcl
+++ modules/modtcl/modtcl.tcl
@@ -34,7 +34,7 @@ set ::botnet-nick ZNC_[GetUsername]
 set ::botnick [GetCurNick]
 set ::server [GetServer]
 set ::server-online [expr [GetServerOnline] / 1000]
-set ::network	[GetServerName]
+set ::network	[GetNetworkName]
 
 # add some eggdrop style procs
 proc putlog message {PutModule $message}
