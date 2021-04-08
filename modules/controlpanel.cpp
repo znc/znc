znc/znc

commit fe8d447a6062c661541361b5587c6172f19bc9ed

listify two-column tables
excluded are the Q and partyline modules, as they are deprecated. There
are some tables that have more than two columns, but could likely be
easily modified, but this will be attempted at a later time.

modules/controlpanel.cpp
@@ -50,6 +50,7 @@ class CAdminMod : public CModule {
         CTable VarTable;
         VarTable.AddColumn(t_s("Type", "helptable"));
         VarTable.AddColumn(t_s("Variables", "helptable"));
+        VarTable.SetStyle(CTable::ListStyle);
         std::map<CString, VCString> mvsTypedVariables;
         for (unsigned int i = 0; i != uSize; ++i) {
             CString sVar = CString(vars[i].name).AsLower();
@@ -119,6 +120,7 @@ class CAdminMod : public CModule {
                 {"ClientEncoding", str},
 #endif
             };
+            PutModule("");
             PrintVarsHelp(sVarFilter, vars, ARRAY_SIZE(vars),
                           t_s("The following variables are available when "
                               "using the Set/Get commands:"));
@@ -142,6 +144,7 @@ class CAdminMod : public CModule {
                 {"TrustAllCerts", boolean},
                 {"TrustPKI", boolean},
             };
+            PutModule("");
             PrintVarsHelp(sVarFilter, nvars, ARRAY_SIZE(nvars),
                           t_s("The following variables are available when "
                               "using the SetNetwork/GetNetwork commands:"));
@@ -155,12 +158,14 @@ class CAdminMod : public CModule {
                                {"InConfig", boolean},
                                {"AutoClearChanBuffer", boolean},
                                {"Detached", boolean}};
+            PutModule("");
             PrintVarsHelp(sVarFilter, cvars, ARRAY_SIZE(cvars),
                           t_s("The following variables are available when "
                               "using the SetChan/GetChan commands:"));
         }
 
         if (sCmdFilter.empty())
+            PutModule("");
             PutModule(
                 t_s("You can use $user as the user name and $network as the "
                     "network name for modifying your own user and network."));
@@ -1279,6 +1284,7 @@ class CAdminMod : public CModule {
         CTable Table;
         Table.AddColumn(t_s("Request", "listctcp"));
         Table.AddColumn(t_s("Reply", "listctcp"));
+        Table.SetStyle(CTable::ListStyle);
         for (const auto& it : msCTCPReplies) {
             Table.AddRow();
             Table.SetCell(t_s("Request", "listctcp"), it.first);
@@ -1493,6 +1499,7 @@ class CAdminMod : public CModule {
         CTable Table;
         Table.AddColumn(t_s("Name", "listmodules"));
         Table.AddColumn(t_s("Arguments", "listmodules"));
+        Table.SetStyle(CTable::ListStyle);
 
         for (const CModule* pMod : Modules) {
             Table.AddRow();

modules/autocycle.cpp
@@ -83,6 +83,7 @@ class CAutoCycleMod : public CModule {
     void OnListCommand(const CString& sLine) {
         CTable Table;
         Table.AddColumn(t_s("Channel"));
+        Table.SetStyle(CTable::ListStyle);
 
         for (const CString& sChan : m_vsChans) {
             Table.AddRow();

modules/certauth.cpp
@@ -169,6 +169,7 @@ class CSSLClientCertMod : public CModule {
 
         Table.AddColumn(t_s("Id", "list"));
         Table.AddColumn(t_s("Key", "list"));
+        Table.SetStyle(CTable::ListStyle);
 
         MSCString::const_iterator it = m_PubKeys.find(GetUser()->GetUsername());
         if (it == m_PubKeys.end()) {

modules/crypt.cpp
@@ -473,6 +473,7 @@ class CCryptMod : public CModule {
         CTable Table;
         Table.AddColumn(t_s("Target", "listkeys"));
         Table.AddColumn(t_s("Key", "listkeys"));
+        Table.SetStyle(CTable::ListStyle);
 
         for (MCString::iterator it = BeginNV(); it != EndNV(); ++it) {
             if (!it->first.Equals(NICK_PREFIX_KEY)) {

modules/fail2ban.cpp
@@ -176,6 +176,7 @@ class CFailToBanMod : public CModule {
         CTable Table;
         Table.AddColumn(t_s("Host", "list"));
         Table.AddColumn(t_s("Attempts", "list"));
+        Table.SetStyle(CTable::ListStyle);
 
         for (const auto& it : m_Cache.GetItems()) {
             Table.AddRow();

modules/lastseen.cpp
@@ -60,6 +60,7 @@ class CLastSeenMod : public CModule {
 
         Table.AddColumn(t_s("User", "show"));
         Table.AddColumn(t_s("Last Seen", "show"));
+        Table.SetStyle(CTable::ListStyle);
 
         for (it = mUsers.begin(); it != mUsers.end(); ++it) {
             Table.AddRow();

modules/log.cpp
@@ -167,6 +167,7 @@ void CLogMod::ListRulesCmd(const CString& sLine) {
     CTable Table;
     Table.AddColumn(t_s("Rule", "listrules"));
     Table.AddColumn(t_s("Logging enabled", "listrules"));
+    Table.SetStyle(CTable::ListStyle);
 
     for (const CLogRule& Rule : m_vRules) {
         Table.AddRow();

modules/notes.cpp
@@ -163,6 +163,7 @@ class CNotesMod : public CModule {
             CTable Table;
             Table.AddColumn(t_s("Key"));
             Table.AddColumn(t_s("Note"));
+            Table.SetStyle(CTable::ListStyle);
 
             for (MCString::iterator it = BeginNV(); it != EndNV(); ++it) {
                 Table.AddRow();

commit 4393b9d906996271344babb819fab53d0729643a

Fix controlpanel output
Accidentally broken in fe8d447a6062c661541361b5587c6172f19bc9ed

modules/controlpanel.cpp
@@ -165,11 +165,12 @@ class CAdminMod : public CModule {
                               "using the SetChan/GetChan commands:"));
         }
 
-        if (sCmdFilter.empty())
+        if (sCmdFilter.empty()) {
             PutModule("");
             PutModule(
                 t_s("You can use $user as the user name and $network as the "
                     "network name for modifying your own user and network."));
+        }
     }
 
     CUser* FindUser(const CString& sUsername) {