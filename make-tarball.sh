174a57a6ab46a5949aa647588c77c6c50192e21e
Alexey Sokolov <alexey+znc@asokolov.org>
2025-04-21 16:38:33 +0100

    fix typo

--- make-tarball.sh
+++ make-tarball.sh
@@ -46,7 +46,7 @@ cp -p third_party/Csocket/Csocket.cc third_party/Csocket/Csocket.h $TMPDIR/$ZNCD
 mkdir -p --mode=0755 $TMPDIR/$ZNCDIR/third_party/cctz
 cp -Rp third_party/cctz/src third_party/cctz/include third_party/cctz/LICENSE.txt $TMPDIR/$ZNCDIR/third_party/cctz/
 mkdir -p --mode=0755 $TMPDIR/$ZNCDIR/third_party/gtest-parallel
-cp -p third_party/gtest-parallel/LICENSE third_party/gtest-parallel/gtest-parallel third_party/gtest-parallel/gtest-parallel.py $TMPDIR/$ZNCDIR/third_party/gtest-parallel/
+cp -p third_party/gtest-parallel/LICENSE third_party/gtest-parallel/gtest-parallel third_party/gtest-parallel/gtest_parallel.py $TMPDIR/$ZNCDIR/third_party/gtest-parallel/
 (
 	cd $TMPDIR2
 	cmake $TMPDIR/$ZNCDIR -DWANT_PERL=yes -DWANT_PYTHON=yes
