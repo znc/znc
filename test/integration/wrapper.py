ca6ae51179c261e5e5f6ec96cc8ff2933cfe3666
Alexey Sokolov <alexey+znc@asokolov.org>
2025-05-04 19:41:11 +0100

    CI: try to fix jenkins from counting every test twice

--- test/integration/wrapper.py
+++ test/integration/wrapper.py
@@ -40,7 +40,11 @@ pid = os.getpid()
 rand = random.randint(0, 1000)
 value = f'{pid}-{rand}'
 for var in ['GTEST_OUTPUT']:
-    if var in os.environ:
+    if test == '':
+        # Avoid outputting xml during test discovery
+        # Otherwise jenkins counts every test twice
+        del os.environ[var]
+    elif var in os.environ:
         os.environ[var] = os.environ[var].replace('XXX', value)
 
 binary = os.environ['INTTEST_BIN']
