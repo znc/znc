znc/znc

commit 5943bc9ed91114d9a389e74f3b54b5d8279268c4

Send several caps in the same CAP REQ
If they got NAKed, then retry them one by one

This should speed up the usual case

Fix #1950

src/IRCSock.cpp
@@ -249,17 +249,42 @@ void CIRCSock::ReadLine(const CString& sData) {
 }
 
 void CIRCSock::SendNextCap() {
-    if (!m_uCapPaused) {
-        if (m_ssPendingCaps.empty()) {
-            // We already got all needed ACK/NAK replies.
-            if (!m_bAuthed) {
-                PutIRC("CAP END");
+    if (m_uCapPaused) {
+        return;
+    }
+
+    if (!m_ssPendingCaps.empty()) {
+        CString sCaps = std::move(*m_ssPendingCaps.begin());
+        m_ssPendingCaps.erase(m_ssPendingCaps.begin());
+        while (!m_ssPendingCaps.empty()) {
+            const CString& sNext = *m_ssPendingCaps.begin();
+            // Old version of cap spec allowed NAK to only contain first 100
+            // symbols of the REQ message.
+            // Alternatively, instead of parsing NAK we could remember the last
+            // REQ sent, but this is simpler, as we need the splitting logic anyway.
+            // TODO: lift this limit to something more reasonable, e.g. 400
+            if (sCaps.length() + sNext.length() > 98) {
+                break;
             }
-        } else {
-            CString sCap = *m_ssPendingCaps.begin();
+            sCaps += " " + sNext;
             m_ssPendingCaps.erase(m_ssPendingCaps.begin());
-            PutIRC("CAP REQ :" + sCap);
         }
+        PutIRC("CAP REQ :" + sCaps);
+        return;
+    }
+
+    if (!m_ssPendingCapsPhase2.empty()) {
+        // Those which NAKed in first phase, try them again, one by one,
+        // to know which of them failed.
+        CString sCap = std::move(*m_ssPendingCapsPhase2.begin());
+        m_ssPendingCapsPhase2.erase(m_ssPendingCapsPhase2.begin());
+        PutIRC("CAP REQ :" + sCap);
+        return;
+    }
+
+    // We already got all needed ACK/NAK replies.
+    if (!m_bAuthed) {
+        PutIRC("CAP END");
     }
 }
 
@@ -436,7 +461,16 @@ bool CIRCSock::OnCapabilityMessage(CMessage& Message) {
         // This should work because there's no [known]
         // capability with length of name more than 100 characters.
         sArgs.Trim();
-        RemoveCap(sArgs);
+        VCString vsCaps;
+        sArgs.Split(" ", vsCaps, false);
+        if (vsCaps.size() == 1) {
+            RemoveCap(sArgs);
+        } else {
+            // Retry them one by one
+            for (CString& sCap : vsCaps) {
+                m_ssPendingCapsPhase2.insert(std::move(sCap));
+            }
+        }
     } else if (sSubCmd == "DEL") {
         VCString vsTokens;
         sArgs.Split(" ", vsTokens, false);

test/integration/tests/core.cpp
@@ -22,6 +22,10 @@
 
 using testing::HasSubstr;
 using testing::ContainsRegex;
+using testing::SizeIs;
+using testing::Lt;
+using testing::Gt;
+using testing::AllOf;
 
 namespace znc_inttest {
 namespace {
@@ -1140,6 +1144,32 @@ TEST_F(ZNCTest, DisableCap) {
     ircd.Write("CAP nick ACK away-notify");
     ASSERT_THAT(client.ReadRemainder().toStdString(),
                 Not(AllOf(HasSubstr("NEW"), HasSubstr("away-notify"))));
+}
+
+TEST_F(ZNCTest, ManyCapsInReq) {
+    constexpr int prefix = std::string_view("CAP NAK :").length();
+    auto znc = Run();
+    auto ircd = ConnectIRCd();
+    ircd.Write(
+        "CAP user LS :away-notify invite-notify extended-join "
+        "userhost-in-names multi-prefix cap-notify "
+        "sasl=ANONYMOUS,EXTERNAL,PLAIN setname tls chghost account-notify "
+        "message-tags batch server-time account-tag echo-message "
+        "labeled-response");
+    QByteArray caps, caps2, caps3;
+    ircd.ReadUntilAndGet("CAP REQ :", caps);
+    caps.remove(0, prefix);
+    EXPECT_THAT(caps.toStdString(), SizeIs(AllOf(Gt(10), Lt(100))));
+    ircd.Write("CAP user NAK :" + caps);
+    ircd.ReadUntilAndGet("CAP REQ :", caps2);
+    caps2.remove(0, prefix);
+    EXPECT_THAT(caps2.toStdString(), SizeIs(AllOf(Gt(10), Lt(100))));
+    ircd.Write("CAP user ACK :" + caps2);
+    // Now it should retry one of the first batch
+    ircd.ReadUntilAndGet("CAP REQ :", caps3);
+    caps3.remove(0, prefix);
+    EXPECT_THAT(caps3.toStdString(), Not(HasSubstr(" ")));
+    EXPECT_TRUE(caps.split(' ').contains(caps3));
 }
 
 }  // namespace

include/znc/IRCSock.h
@@ -226,6 +226,7 @@ class CIRCSock : public CIRCSocket {
     unsigned int m_uCapPaused;
     SCString m_ssAcceptedCaps;
     SCString m_ssPendingCaps;
+    SCString m_ssPendingCapsPhase2;
     MCString m_msCapLsValues;
     time_t m_lastCTCP;
     unsigned int m_uNumCTCP;

commit aaeab22ea3d4df73e8ee0d0c2870b6cd650d0076

Fix processing of multi-token CAP ACK
I broke it in 5943bc9ed91114d9a389e74f3b54b5d8279268c4 while fixing #1950

src/IRCSock.cpp
@@ -450,17 +450,19 @@ bool CIRCSock::OnCapabilityMessage(CMessage& Message) {
             }
         }
     } else if (sSubCmd == "ACK") {
-        sArgs.Trim();
-        IRCSOCKMODULECALL(OnServerCapResult(sArgs, true), NOTHING);
-        auto it = mSupportedCaps.find(sArgs);
-        if (it != mSupportedCaps.end()) {
-            it->second(true);
+        VCString vsCaps;
+        sArgs.Split(" ", vsCaps, false);
+        for (CString& sCap : vsCaps) {
+            IRCSOCKMODULECALL(OnServerCapResult(sCap, true), NOTHING);
+            auto it = mSupportedCaps.find(sCap);
+            if (it != mSupportedCaps.end()) {
+                it->second(true);
+            }
+            m_ssAcceptedCaps.insert(std::move(sCap));
         }
-        m_ssAcceptedCaps.insert(sArgs);
     } else if (sSubCmd == "NAK") {
         // This should work because there's no [known]
         // capability with length of name more than 100 characters.
-        sArgs.Trim();
         VCString vsCaps;
         sArgs.Split(" ", vsCaps, false);
         if (vsCaps.size() == 1) {

test/integration/tests/core.cpp
@@ -21,6 +21,7 @@
 #include "znctestconfig.h"
 
 using testing::HasSubstr;
+using testing::Contains;
 using testing::ContainsRegex;
 using testing::SizeIs;
 using testing::Lt;
@@ -1169,7 +1170,7 @@ TEST_F(ZNCTest, ManyCapsInReq) {
     ircd.ReadUntilAndGet("CAP REQ :", caps3);
     caps3.remove(0, prefix);
     EXPECT_THAT(caps3.toStdString(), Not(HasSubstr(" ")));
-    EXPECT_TRUE(caps.split(' ').contains(caps3));
+    EXPECT_THAT(caps.split(' '), Contains(caps3));
 }
 
 TEST_F(ZNCTest, JoinWhileRegistration) {

test/integration/tests/modules.cpp
@@ -305,9 +305,9 @@ TEST_P(SaslModuleTest, Test) {
     client.Write("znc jump");
     ircd = ConnectIRCd();
     ircd.ReadUntil("CAP LS");
-    ircd.Write("CAP * LS :sasl");
-    ircd.ReadUntil("CAP REQ :sasl");
-    ircd.Write("CAP * ACK :sasl");
+    ircd.Write("CAP * LS :away-notify sasl");
+    ircd.ReadUntil("CAP REQ :away-notify sasl");
+    ircd.Write("CAP * ACK :away-notify sasl");
     ircd.ReadUntil("AUTHENTICATE EXTERNAL");
     ircd.Write(":server 904 *");
     ircd.ReadUntil("AUTHENTICATE PLAIN");