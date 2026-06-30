znc/znc

commit 94bcf919e1163564e637df8d437585bc7b55e7aa

Fix build error with OpenSSL 4 (#2024)
More details here https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1138356

src/SSLVerifyHost.cpp
@@ -306,8 +306,8 @@ typedef enum {
 static HostnameValidationResult matches_common_name(const char* hostname,
                                                     const X509* server_cert) {
     int common_name_loc = -1;
-    X509_NAME_ENTRY* common_name_entry = nullptr;
-    ASN1_STRING* common_name_asn1 = nullptr;
+    const X509_NAME_ENTRY* common_name_entry = nullptr;
+    const ASN1_STRING* common_name_asn1 = nullptr;
     CONST_ASN1_STRING_DATA char* common_name_str = nullptr;
 
     // Find the position of the CN field in the Subject field of the certificate

src/Utils.cpp
@@ -129,7 +129,8 @@ void CUtils::GenerateCert(FILE* pOut) {
     sEmailAddr += "@";
     sEmailAddr += sHostName;
 
-    X509_NAME* pName = X509_get_subject_name(pCert.get());
+    X509_NAME* pName = X509_NAME_new();
+    if (!pName) return;
     X509_NAME_add_entry_by_txt(pName, "OU", MBSTRING_ASC,
                                (unsigned char*)pLogName, -1, -1, 0);
     X509_NAME_add_entry_by_txt(pName, "CN", MBSTRING_ASC,
@@ -137,7 +138,9 @@ void CUtils::GenerateCert(FILE* pOut) {
     X509_NAME_add_entry_by_txt(pName, "emailAddress", MBSTRING_ASC,
                                (unsigned char*)sEmailAddr.c_str(), -1, -1, 0);
 
+    X509_set_subject_name(pCert.get(), pName);
     X509_set_issuer_name(pCert.get(), pName);
+    X509_NAME_free(pName);
 
     if (!X509_sign(pCert.get(), pKey.get(), EVP_sha256())) return;
 

commit 0146161aa0ee39409183ec4c45d7c14d905a940b

Fix build with LibreSSL >= 2.7.0 by using native ASN1_STRING_get0_data
LibreSSL >= 2.7.0 has its own ASN1_STRING_get0_data() that accepts
const ASN1_STRING*, unlike the ASN1_STRING_data() fallback used in the
#else branch. The const-qualifier loss error was introduced by commit
94bcf919 which made common_name_asn1 const for OpenSSL 4 compatibility.

src/SSLVerifyHost.cpp
@@ -21,6 +21,8 @@
 #ifdef HAVE_LIBSSL
 #if defined(OPENSSL_VERSION_NUMBER) && !defined(LIBRESSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100007
 # define CONST_ASN1_STRING_DATA const /* 1.1.0-pre7: openssl/openssl@17ebf85abda18c3875b1ba6670fe7b393bc1f297 */
+#elif defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20700000L
+# define CONST_ASN1_STRING_DATA const
 #else
 # define ASN1_STRING_get0_data( x ) ASN1_STRING_data( x )
 # define CONST_ASN1_STRING_DATA